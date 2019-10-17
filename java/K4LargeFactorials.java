820b692205d724cae95529b56210b029d0aee954
Eric <22215921+ericywl@users.noreply.github.com>
2019-10-17 14:29:15 +0800

    Add LargeFactorials

---
+++ java/K4LargeFactorials.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class K4LargeFactorials {
+    private static List<Integer> Multiply(List<Integer> numList, int mult) {
+        int carry = 0;
+        List<Integer> result = new ArrayList<Integer>();
+
+        for (int num : numList) {
+            int temp = num * mult + carry;
+            result.add(temp % 10);
+            carry = temp / 10;
+        }
+        
+        while (carry != 0) {
+            result.add(carry % 10);
+            carry /= 10;
+        }
+
+        return result;
+    }
+
+    public static String Factorial(int n) {
+        List<Integer> result = Arrays.asList(new Integer[] { 1 });
+        while (n > 1) {
+            result = Multiply(result, n);
+            n--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = result.size() - 1; i >= 0; i--) {
+            sb.append(result.get(i));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Factorial(20));
+    }
+}
